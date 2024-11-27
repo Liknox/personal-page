a5f716361dd191d4b8c7a02fcbb52729841e9021
Nazar Koval <n.kovaldev@gmail.com>
2024-11-27 10:02:22 +0200

    feat: implement initialAnimation context

---
+++ src/app/context/initialAnimation/useInitialAnimation.ts
@@ -0,0 +1,8 @@
+import { useContext } from "react"
+import { InitialAnimationContext } from "./context"
+
+const useInitialAnimation = () => {
+   return useContext(InitialAnimationContext)
+}
+
+export { useInitialAnimation }
