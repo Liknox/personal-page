ee8e4c1ae47816aad52ecf57bd961f9deb810e47
Nazar Koval <n.kovaldev@gmail.com>
2024-11-27 09:12:34 +0200

    template: container layout

--- configs/build/vite.config.ts
+++ configs/build/vite.config.ts
@@ -1,4 +1,3 @@
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,39 @@ import { routesPaths } from "@app/configs/router"
 import { createFileRoute } from "@tanstack/react-router"
 import styled from "styled-components"
 
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+	justify-content: flex-start;
+	align-items: flex-start;
+	align-content: flex-start;
+	width: 100%;
+`
+
+const TopContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: flex-start;
+	align-items: flex-start;
+	align-content: flex-start;
+	width: 100%;
+
+	@media (max-width: 768px) {
+		flex-direction: column;
+	}
+`
+
 const Home = () => {
-   return <p>Home</p>
+	return (
+		<Container>
+			<TopContainer>
+			</TopContainer>
+		</Container>
+	)
 }
 
 export const Route = createFileRoute(routesPaths.home)({
-   component: () => <Home />,
+	component: () => <Home />,
 })
