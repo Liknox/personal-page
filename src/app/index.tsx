efabf9c4c5d1bc5025ab7e5e9962dcea5d59da01
Nazar Koval <n.kovaldev@gmail.com>
2024-11-28 17:10:47 +0200

    feat: implement initialAnimationContext

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,14 @@
+import { InitialAnimationContextProvider } from "./providers/initialAnimation"
 import { Router } from "./providers/router"
 import { GlobalStyles } from "./styles/globalStyles"
 
 const App = () => {
    return (
       <>
+      <InitialAnimationContextProvider>
          <GlobalStyles />
          <Router />
+      </InitialAnimationContextProvider>
       </>
    )
 }
