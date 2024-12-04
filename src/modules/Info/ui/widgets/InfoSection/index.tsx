d7ad57f68b87f249fa6c097e79d4dbab6be7d16e
Nazar Koval <n.kovaldev@gmail.com>
2024-12-04 20:09:54 +0200

    feat: create Info module
    * format files
    * added required exports by api

---
+++ src/modules/Info/ui/widgets/InfoSection/index.tsx
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion"
+import styled from "styled-components"
+
+const Container = styled(motion.div)`
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+	justify-content: center;
+	align-items: center;
+	align-content: center;
+	width: calc(100vw / 2);
+	border: 1px solid var(--paper-border-secondary-color);
+	height: calc(100vh - 100vh / 4);
+	transition: border var(--timing-s) ease;
+	overflow: hidden;
+	padding: var(--spacing-m);
+
+	&:hover {
+		border: 1px solid var(--paper-border-color);
+	}
+
+	@media (max-width: 768px) {
+		width: 100%;
+		height: 100dvh;
+	}
+`
+
+const InfoSection = () => {
+	return <Container></Container>
+}
+
+export { InfoSection }
