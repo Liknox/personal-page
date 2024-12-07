5714e718bbc601983f4e6c185a994202edd1ccdc
Nazar Koval <n.kovaldev@gmail.com>
2024-12-07 20:23:51 +0200

    fix: types

--- src/app/types/BackgroundCircle/types.ts
+++ src/app/types/BackgroundCircle/types.ts
@@ -1,19 +1,25 @@
 type Opacity = {
-   opacity: number
+	opacity: number
 }
 
 type Resolutions = {
-   x: number
-   y: number
+	x: number
+	y: number
 }
 
 type Duration = {
-   duration: number
+	duration: number
+}
+
+type Spring = {
+	type: string
+	stiffness: number
+	damping: number
 }
 
 export type BackgroundCircle = {
-   style: Resolutions
-   animate: Opacity
-   initial: Opacity
-   transition: Duration
+	style: Resolutions
+	animate: Opacity
+	initial: Opacity
+	transition: Duration | Spring
 }
