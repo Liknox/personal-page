c85671db5bcec541220b49d86aac710206c17e29
Nazar Koval <n.kovaldev@gmail.com>
2024-12-07 19:02:50 +0200

    feat: take out BackgroundCircle as a separate component
    * implement styles to that
    * take out api to access this component

---
+++ src/ui/BackgroundCircle/ui/index.tsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion"
+import styled from "styled-components"
+
+const BackgroundCircleStyles = styled(motion.div)`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 200px;
+	height: 200px;
+	border-radius: 50%;
+	background: radial-gradient(
+		circle,
+		color-mix(in srgb, var(--primary-color) 80%, transparent 100%),
+		color-mix(in srgb, var(--primary-dark-color) 80%, transparent 100%)
+	);
+	filter: blur(50px);
+	pointer-events: none;
+`
+
+const BackgroundCircle = (props: any) => {
+   return <BackgroundCircleStyles {...props} />
+}
+
+export { BackgroundCircle }
