3875fdd4a86bc1ff3e4a361947023b8d98f28876
Nazar Koval <n.kovaldev@gmail.com>
2024-12-08 21:44:15 +0200

    feat: cutie markup for __404 page

--- src/pages/__404.tsx
+++ src/pages/__404.tsx
@@ -1,4 +1,6 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute, Link } from "@tanstack/react-router"
+import { motion } from "framer-motion"
+import { useMemo, useState } from "react"
 import styled from "styled-components"
 
 export const Route = createFileRoute("/__404")({
@@ -14,7 +16,51 @@ const Container = styled.div`
 	align-content: center;
 	height: 100%;
 `
+const Button = styled(motion.button)`
+	color: var(--secondary-color);
+	padding: var(--spacing-xs) var(--spacing-m);
+	border-radius: var(--border-radius-xl);
+	cursor: pointer;
+	transition: background var(--timing-s) ease;
+	position: relative;
+	border-radius: 9999px;
+	padding: 0.375rem 0.75rem;
+	font-size: 0.875rem;
+	font-weight: 500;
+	outline: 2px solid transparent;
+	-webkit-tap-highlight-color: transparent;
+	background: var(--secondary-color);
+	margin-top: var(--spacing-xs);
+
+	a {
+		color: black;
+		text-decoration: none;
+	}
+`
 
 const NotFound = () => {
-   return <Container>Page Not Found</Container>
+   const [isHovered, setIsHovered] = useState(false)
+
+   const handleHoverStart = () => setIsHovered(true)
+   const handleHoverEnd = () => setIsHovered(false)
+
+   const iconProps = useMemo(
+      () => ({
+         initial: { opacity: 0 },
+         animate: {
+            opacity: isHovered ? 1 : 0.5,
+         },
+         transition: { type: "spring", stiffness: 500, damping: 30 },
+      }),
+      [isHovered],
+   )
+
+   return (
+      <Container>
+         Page Not Found
+         <Button {...iconProps} onHoverStart={handleHoverStart} onHoverEnd={handleHoverEnd}>
+            <Link to="/">Return Home</Link>
+         </Button>
+      </Container>
+   )
 }
