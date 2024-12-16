7435e4c68fb11e0bd5195ecbb907c84b068736e1
Nazar Koval <n.kovaldev@gmail.com>
2024-12-16 21:43:11 +0200

    fix: small fixes and other minor updates
    * remove default vite.svg
    * update welcome section's info

--- src/modules/Info/ui/widgets/InfoSection/WelcomeSection.tsx
+++ src/modules/Info/ui/widgets/InfoSection/WelcomeSection.tsx
@@ -40,15 +40,12 @@ const WelcomeSection = memo(({ isHovered }: Props) => {
             Hi there,
          </AnimatedText>
          <AnimatedText isHovered={isHovered} delay={0.2}>
-            I'm Nazar Koval, a Software Engineer.
+            I'm Nazar Koval, a Software Engineer,
          </AnimatedText>
          <AnimatedText isHovered={isHovered} delay={0.3}>
-            I craft perfect business solutions,
-         </AnimatedText>
-         <AnimatedText isHovered={isHovered} delay={0.4}>
             Burn my eyes out with colorful letters in the IDE.
          </AnimatedText>
-         <AnimatedText isHovered={isHovered} delay={0.5}>
+         <AnimatedText isHovered={isHovered} delay={0.4}>
             Let's work together!
          </AnimatedText>
       </>
